1a7771b376ad8aa19de6b89e974677d9e2e48ab8
lauraTchaly <lauramanes@outlook.com>
2022-04-06 16:41:00 -0300

    segunda aula

---
+++ page01.php
@@ -0,0 +1,19 @@
+<?php
+// cria uma variável
+$carro = "Fusca";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- ECHO = enviar para a tela -->
+    <?php echo $carro ?>
+</body>
+</html>
