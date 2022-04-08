ab7cf280d39c63c00a57fed7b3795852eba2942c
lauraTchaly <lauramanes@outlook.com>
2022-04-08 16:46:48 -0300

    aula
    Aula PHP 4

---
+++ _footer.php
@@ -0,0 +1,19 @@
+</main>
+
+<footer>
+
+    <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney"></i></a>
+
+    <div>© Copyright 2022 Vitugo.</div>
+
+    <a href="#top" title="Topo da página"><i class="fa-solid fa-circle-up"></i></a>
+
+</footer>
+      
+</div>
+
+</body>
+
+</html>
+
+        
---
+++ modelo.php
@@ -0,0 +1,51 @@
+
+<?php
+
+// inclui o cabeçalho da página
+require('_header.php')
+
+
+?>
+            <section>
+
+                <h2>Título da página</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, iste aliquam aperiam voluptatem
+                    molestias nemo odit unde modi cupiditate exercitationem doloremque quaerat soluta rerum quidem
+                    dignissimos officiis sapiente, aut alias!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio soluta voluptatum consequatur
+                    voluptatibus cupiditate temporibus qui, nostrum deserunt minus laudantium in officia rem dignissimos
+                    facilis modi culpa error aliquam? Quam?</p>
+
+            </section>
+
+            <aside>
+
+                <h3>Lateral</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, aperiam corporis culpa consequatur
+                    iusto.</p>
+
+            </aside>
+
+        </main>
+
+        <footer>
+
+            <a href="/" title="Página inicial"><i class="fa-solid fa-house-chimney"></i></a>
+             <!-- &copy; = cria o símbulo do copyright -->
+            <div>© Copyright 2022 Vitugo.</div>
+
+            <a href="#top" title="Topo da página"><i class="fa-solid fa-circle-up"></i></a>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
+
+<?php
+
+require('_footer.php')
+?>
+        
