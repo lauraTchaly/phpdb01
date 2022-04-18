618cd588ead5d0f8a9fbfbd18e436a913722be1d
lauraTchaly <lauramanes@outlook.com>
2022-04-18 16:41:00 -0300

    aula6

--- _config.php
+++ _config.php
@@ -15,3 +15,23 @@ $db = array(
     "password" => ""
 );
 $conn = new mysqli($db["hostname"], $db["username"], $db["password"], $db["database"]);
+
+/**
+ * Seta transações entre MySQL/MariaDB e PHP para UTF-8.
+ */
+$conn->query("SET NAMES 'utf8'");
+$conn->query('SET character_set_connection=utf8');
+$conn->query('SET character_set_client=utf8');
+$conn->query('SET character_set_results=utf8');
+
+/**
+ * Seta dias da semana e meses do MySQL/MariaDB para "português do Brasil".
+ */
+$conn->query('SET GLOBAL lc_time_names = pt_BR');
+$conn->query('SET lc_time_names = pt_BR');
+
+/**
+ * Define o fuso horário (opcional + recomendado).
+ */
+date_default_timezone_set('America/Sao_Paulo');
+
